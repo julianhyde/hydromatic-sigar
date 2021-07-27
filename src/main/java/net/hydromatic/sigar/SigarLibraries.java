cfa68e8ff496fff2fabc37f10f28d9ea28883360
Julian Hyde <jhyde@apache.org>
2021-07-27 10:07:12 -0700

    Add sigar-1.6.4 binaries

--- src/main/java/net/hydromatic/sigar/SigarLibraries.java
+++ src/main/java/net/hydromatic/sigar/SigarLibraries.java
@@ -18,9 +18,37 @@
  */
 package net.hydromatic.sigar;
 
+import java.net.URL;
+import java.util.function.Function;
+
 /** Libraries (DLLs and Shared Object files) for Hyperic Sigar. */
 public enum SigarLibraries {
-  INSTANCE
+  INSTANCE;
+
+  public static final String SIGAR_PATH = "org.hyperic.sigar.path";
+
+  public static final String SIGAR_LIB_PATH =
+      "/hyperic-sigar-1.6.4/sigar-bin/lib/";
+
+  /** Returns the directory in which the binaries reside, as a URL. */
+  public URL resourceUrl() {
+    return SigarLibraries.class.getResource(SIGAR_LIB_PATH);
+  }
+
+  /** Loads the library, if it is not already loaded. */
+  public boolean load(Function<URL, String> resolver) {
+    if (!isEmpty(System.getProperty(SIGAR_PATH))) {
+      return false;
+    }
+    final URL sigarPathUrl = resourceUrl();
+    final String path = resolver.apply(sigarPathUrl);
+    System.setProperty(SIGAR_PATH, path);
+    return true;
+  }
+
+  private boolean isEmpty(String s) {
+    return s == null || s.equals("");
+  }
 }
 
 // End SigarLibraries.java
